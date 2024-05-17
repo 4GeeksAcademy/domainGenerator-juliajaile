cd3f1077b721643b0467b31eb560ddf98c05edd1
Júlia Jaile Flores <73072636+JJFlor@users.noreply.github.com>
2024-05-17 11:14:04 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,27 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+//write your code here
+function generateAllDomainCombinations() {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".com", ".net", ".us", ".io"];
+
+  for (let pronounItem of pronoun) {
+    for (let adjectiveItem of adj) {
+      for (let nounItem of noun) {
+        for (let extensionItem of extension) {
+          console.log(
+            `${pronounItem} ${adjectiveItem} ${nounItem} ${extensionItem}`
+          );
+        }
+      }
+    }
+  } //return `${pronounItem} ${adjectiveItem} ${nounItem} ${extensionItem}`;
+}
+
+generateAllDomainCombinations();
+
+//let domainElement = document.getElementById("domain");
+//domainElement.innerHTML = generateAllDomainCombinations();
